dbe46f20924b6a10d1e6b0fb5bb6518eac6b1475
evanhagood <ehagood1@asu.edu>
2024-03-25 20:15:58 -0400

    uhhhhh part 16

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -347,9 +347,7 @@ class WebServer {
             boolean popular = Boolean.parseBoolean(query_pairs.get("popular"));
             String gender = query_pairs.get("gender");
 
-            if(!gender.equalsIgnoreCase("boy") || !gender.equalsIgnoreCase("girl")) {
-              System.out.println("WHAT THE FUCK IS GOING ON");
-              System.out.println(gender);
+            if(!gender.equalsIgnoreCase("boy") && !gender.equalsIgnoreCase("girl")) {
               throw new IllegalArgumentException(); // just go to catch block, no real logging here
             }
 
